6ccd29fe7d92b7c5821e75fe5617eeb039a149eb
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-01-18 18:03:13 +0100

    Started landing page, added images

---
+++ frontend/src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import clock from './images/clock2x.svg';
+
+function App() {
+    return (
+<div>
+    <div>
+      <h1>manage your time based on your personality and your preferences</h1>
+      <h2>Take a quiz and personalize your time management</h2>
+    </div>
+    <div>
+      <button className="primaryButton" onClick={() => console.log('Click')}>
+            Take a Quiz
+      </button>
+      <p>Learn about your type</p>
+    </div>
+    <img src={clock} alt="Clock between day and night}" />
+</div>
+    );
+}
+
+
+export default App;
