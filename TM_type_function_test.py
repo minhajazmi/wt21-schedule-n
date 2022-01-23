5f541149ac5354a1cf914f37ce7998e775e05559
Xinhao221b <xinhao.wang221b@gmail.com>
2022-01-23 04:04:33 +0100

    add time management convertion function and testing script

---
+++ TM_type_function_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 03:47:19 2022
+
+@author: xinhao
+"""
+
+#%% call for the function to get user Time management type
+
+user_answer = 'quizdata_edited.json' # here the file name will be dependent on our real file name
+
+from get_TM_type import get_TM_type
+
+TM_type = get_TM_type(user_answer)
