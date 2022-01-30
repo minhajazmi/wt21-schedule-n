e3a68f44160334211c9e00e8ae21848e197d4a0c
Angelina Blumenthal <angelina.blumenthal@gmail.com>
2022-01-30 17:09:40 +0100

    fix: api adress 4 options

--- support material/Quiz.js
+++ support material/Quiz.js
@@ -44,7 +44,7 @@ function Quiz() {
   const onSubmit = data => {
   console.log(errors);
 
-  axios.post('http://localhost:5000/questions', JSON.stringify(data))
+  axios.post('http://localhost:4000/api/questions', JSON.stringify(data))
        .then(response => {
       setResult(types.find(type => type.res === response.data));
       })
