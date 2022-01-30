7a6607f9bdf6fbcd1253b1a3a7ace6a1515e41b1
Sullyheart <59294785+Sullyheart@users.noreply.github.com>
2022-01-30 13:00:11 +0100

    mend

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 const questionController = require("../controller/questions.js");
 
 // routes.get('',)
-routes.post("/question", questionController.saveQuestions);
-routes.get("/question", questionController.getAnswers);
+routes.post("/questions", questionController.saveQuestions);
+routes.get("/questions", questionController.getAnswers);
 
 module.exports = routes;
