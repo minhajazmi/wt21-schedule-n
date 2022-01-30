383551ae855ccb6b76eb8b1beb33e50c21b99f70
Xinhao221b <xinhao.wang221b@gmail.com>
2022-01-30 12:00:05 +0100

    testing_save_to_server_function

--- TM_type_function_test.py
+++ TM_type_function_test.py
@@ -13,3 +13,27 @@ user_answers = 'quizdata.json' # here the file name will be dependent on our rea
 from get_TM_type_as_Json import get_TM_type_as_Json
 
 get_TM_type_as_Json(user_answers)
+
+#%% save results to Github
+
+#from github import Github
+#
+#token = "YOUR_TOKEN"
+#
+#repo = "bertrandmartel/test-repo"
+#path = "data.json"
+#
+## if using username and password
+##g = Github("user", "password")
+#
+#g = Github(token)
+#
+#data = open("data.json", "r").read()
+#
+#repo = g.get_repo(repo)
+#repo.create_file(
+#    path = path, 
+#    message = "add new file", 
+#    content = data, 
+#    branch = "master"
+#)
