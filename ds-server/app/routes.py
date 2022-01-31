1674fb483f666f390f3e20498b67ebcf9b61ad97
Sullyheart <59294785+Sullyheart@users.noreply.github.com>
2022-01-31 13:13:12 +0100

    Add docker file

--- ds-server/app/routes.py
+++ ds-server/app/routes.py
@@ -7,38 +7,33 @@ from app import app
 @app.route('/api/process', methods=['POST'])
 def processData():
     data = request.json
-    return get_TM_type_as_Json(data)
+    return get_TM_type(data)
 
 """
-Created on Sun Jan 23 03:30:27 2022
+Created on Sun Jan 30 17:55:09 2022
 
 @author: xinhao
 
-This function convert user input into time management (TM) types
+This function convert user input (23 question, 5 option) into time management (TM) types
 
 Input:  - .json file of user TM questionnaire answer
-Output: - a string of the name of the TM type ('Hooper'/'Perfectionist'/'Implusive'
-        /'Cliff hanger'/'Big idea'/'Hyperfocus')
+Output: - a dictionary object containing the name of the TM type, e.g., 
+        {'TM_type':'Hooper'}, {'TM_type':'Perfectionist'}, {'TM_type':'Implusive'},
+        {'TM_type':'Cliff hanger'}, {'TM_type':'Big idea'}, {'TM_type':'Hyperfocus'}
 """
 
 # get TM_type.py
 
-def get_TM_type_as_Json(user_answers):
+def get_TM_type(user_answers):
     
     #%% import libraries
     # This is the convention used to import Pandas.
     import pandas as pd
     import numpy as np
-    import json
+  #  import json
 
     #%% load data
     # These commands load the survey data 
-    
-    # Open JSON file
-    # f = open(user_answers)
-    
-    # load json file as a dictionary
-    # data_dict = json.load(f, strict=False)
     
     # convert dictionary to DataFrame
     df = pd.DataFrame([user_answers])
@@ -52,16 +47,16 @@ def get_TM_type_as_Json(user_answers):
     
     
     def text_to_score(a):
-         if a == "completely-disagree":
+         if a == "completelyDisagree":
              return 1
-         elif a == "I-disagree":
+         elif a == "IDisagree":
              return 2
-         elif a == "I-disgree":
-             return 2
-         elif a == "I-agree":
+         elif a == "neutral":
              return 3
-         elif a == "completely-agree":
-             return 4 
+         elif a == "IAgree":
+             return 4
+         elif a == "completelyAgree":
+             return 5 
     
     
     #%% data cleaning      
@@ -70,35 +65,35 @@ def get_TM_type_as_Json(user_answers):
     
     
     #%% calculate the average score for each type (23 question FINAL version)
-    # hooper        = (Q1 + Q4 + Q6 + Q14 + Q16 + (5 - Q19)) / 6 - 2.5
-    # Perfectionist = (Q2 + Q5 + Q7 + Q9 + Q15 + (5 - Q17)) / 6 - 2.5
-    # cliff_hanger  = (Q12 + Q20 + Q21 + Q22 + (5 - Q8)) / 5 - 2.5
-    # impulsive     = (Q3 + Q4 + Q16 + Q18 + (5 - Q10)) / 5 - 2.5
-    # big_idea      = (Q11 + Q21 + Q23 + (5 - Q7)) / 4 - 2.5
-    # hyperfocus    = (Q7 + Q13 + Q24 + (5 - Q1) + (5 - Q6)) / 5 - 2.5
+    # hooper        = (Q1 + Q4 + Q6 + Q14 + Q16 + (6 - Q19)) / 6 - 3
+    # Perfectionist = (Q2 + Q5 + Q7 + Q9 + Q15 + (6 - Q17)) / 6 - 3
+    # cliff_hanger  = (Q12 + Q20 + Q21 + Q22 + (6 - Q8)) / 5 - 3
+    # impulsive     = (Q3 + Q4 + Q16 + Q18 + (6 - Q10)) / 5 - 3
+    # big_idea      = (Q11 + Q21 + Q23 + (6 - Q7)) / 4 - 3
+    # hyperfocus    = (Q7 + Q13 + Q24 + (6 - Q1) + (6 - Q6)) / 5 - 3
     
     type_num_data['hopper_score'] = (type_num_data.iloc[0,0] + type_num_data.iloc[0,3]  
         + type_num_data.iloc[0,5] + type_num_data.iloc[0,13] + type_num_data.iloc[0,15] 
-        + (5 - type_num_data.iloc[0,18])) / 6 - 2.5
+        + (6 - type_num_data.iloc[0,18])) / 6 - 3
     
     type_num_data['perfectionist_score'] = (type_num_data.iloc[0,1] + type_num_data.iloc[0,4]  
         + type_num_data.iloc[0,6] + type_num_data.iloc[0,8] + type_num_data.iloc[0,14] 
-        + (5 - type_num_data.iloc[0,16])) / 6 - 2.5
+        + (6 - type_num_data.iloc[0,16])) / 6 - 3
     
     type_num_data['cliff_hanger_score'] = (type_num_data.iloc[0,11] + type_num_data.iloc[0,19]  
         + type_num_data.iloc[0,20] + type_num_data.iloc[0,21] 
-        + (5 - type_num_data.iloc[0,7])) / 5 - 2.5
+        + (6 - type_num_data.iloc[0,7])) / 5 - 3
     
     type_num_data['impulsive_score'] = (type_num_data.iloc[0,2] + type_num_data.iloc[0,3]  
         + type_num_data.iloc[0,15] + type_num_data.iloc[0,17] 
-        + (5 - type_num_data.iloc[0,9])) / 5 - 2.5
+        + (6 - type_num_data.iloc[0,9])) / 5 - 3
         
     type_num_data['big_idea_score'] = (type_num_data.iloc[0,10] + type_num_data.iloc[0,20]  
-        + type_num_data.iloc[0,22] + (5 - type_num_data.iloc[0,6])) / 4 - 2.5
+        + type_num_data.iloc[0,22] + (6 - type_num_data.iloc[0,6])) / 4 - 3
     
     type_num_data['hyperfocus_score'] = (type_num_data.iloc[0,6] + type_num_data.iloc[0,12]  
-        + type_num_data.iloc[0,23] + (5 - type_num_data.iloc[0,0]) 
-        + (5 - type_num_data.iloc[0,5])) / 5 - 2.5
+        + type_num_data.iloc[0,23] + (6 - type_num_data.iloc[0,0]) 
+        + (6 - type_num_data.iloc[0,5])) / 5 - 3
     
     #%% get the result from the average score
     
@@ -120,16 +115,12 @@ def get_TM_type_as_Json(user_answers):
     #create new column in DataFrame that displays results of comparisons
     type_num_data['winner_type'] = np.select(conditions, choices, default='mixed type')
     
-    #%% return TM type of user as a string
-    
-    # return type_num_data['winner_type'][0]
 
-    #%% save TM type of user as a Json file
+    #%% save TM type of user as a dictionary object
     
     user_TM_type = {
             'TM_type': type_num_data['winner_type'][0]
             }
     
     return user_TM_type
-
    
